559b1ed9938ff70218edc77c9ebb510d5fd2aaa7
yg.song <insaint03@gmail.com>
2020-10-02 18:42:41 +0900

    tmp

--- downdrop.js
+++ downdrop.js
@@ -40,7 +40,7 @@
             let rss = [];
             let rs = [];
 
-            while((ts_cursor+interval) <= ts_threshold) {
+            while(ts_cursor <= ts_threshold) {
                 let date_starts = (new Date(ts_cursor)).toISOString().split('T')[0];
                 let date_ends = (new Date(Math.min(ts_threshold, ts_cursor + interval))).toISOString().split('T')[0];
                 let resp = await promised_get(`${endpoint}`, Object.assign(params, {
@@ -90,8 +90,66 @@
             return rets;
         },
 
-        'coupeats': (opts) => {
-            return opts;
+        'store.coupangeats.com': async (opts) => {
+            const endpoint = '/api/v1/merchant/web';
+            let store_list = ()=> new Promise((rs,rj) => {
+                $.ajax({method: 'GET', url: `${endpoint}/stores`, dataType: 'json',  
+                    success: (resp) => { rs(resp.data); },
+                    error: rj,
+                });
+            });
+            let order_list = (store_id, date_start, date_ends, page)=> new Promise((rs, rj) => {
+                $.ajax({method: 'POST', url: `${endpoint}/order/condition`, dataType: 'json',
+                    data: JSON.parse({
+                        storeId: store_id,
+                        startDate: new Date(`${date_start}T00:00:00`).getTime(),
+                        endDate: new Date(`${date_ends}T00:00:00`).getTime()-1,
+                        pageNumber: page || 0,
+                        pageSize: 50,
+                        success: (resp) => { rs(resp.data) },
+                        error: rj,
+                    })});
+            });
+
+            // load store list first
+            let stores = await store_list();
+            stores.filter((store) => 
+                opts.branches.reduce((filtered, branch) => {
+                    return filtered || store.name.replace(/\w/g, '').includes(branch);
+                }, false));
+
+            let rets = [];
+            stores.forEach((store) => {
+                let rs = [];
+                let totals = 0;
+                let page = 0;
+                do {
+                    let resp = await order_list(store.id, opts.start_date, opts.end_date, page);
+                    rs = rs.concat(resp.content.reduce((row, order)=> {
+                        let items = order.items;
+                        if(order.canceledItems) {
+                            let filters = order.canceledItems.map((ci)=>ci.orderItemId);
+                            items = items.filter((item)=>!filters.includes(item.orderItemId))
+                        }
+                        return row.concat(items.map((item)=>[
+                            'CoupangEats',
+                            order.abbrOrderId,
+                            new Date(order.createdAt).toISOString().replace(/[TZ]/g, ' ').trim(),
+                            store.name,
+                            item.name,
+                            item.quantity,
+                            item.unitSalePrice,
+                            0,
+                            order.actuallyAmount,
+                        ]));
+                    }, []));
+                    totals = resp.totalElements;
+                } while(rs.length < totals);
+                rets = rets.concat(rs);
+            });
+
+            window.console.log(rets);
+            return rets;
         },
 
         'unos': (opts) => {
